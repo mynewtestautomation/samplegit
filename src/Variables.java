afe324e0699123c5aaf0e55f02072cf3f6048a08
sangeetha <sangee.m3@gmail.com>
2018-03-13 17:00:51 -0500

    first commit

---
+++ src/Variables.java
@@ -0,0 +1,12 @@
+
+public class Variables {
+public static int wheels=0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int speed=65;
+String limit="highway";
+String name= Integer.toString(speed)+ limit;
+System.out.println("output is :" +name);
+	}
+
+}
